d78748c1b5308ad6778dee9decb9be2ec334f723
davidpombar <david.pombar@gmail.com>
2015-03-15 22:27:03 +0100

    Rails New NovoFund, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NovoFund::Application.config.secret_key_base = '0737e6033b204d7e8804c05751a5eb329086fea10987da525d46262457a72d486b675d4600db927cb56f95a7a2aaa28548d66de0400ab928bc15c4d1bad36f98'
